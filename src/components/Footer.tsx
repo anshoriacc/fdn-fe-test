c5dfd1e4b0aa6129981a5bd74d1f682819a472cf
achmad anshori <utd.rik@gmail.com>
2022-11-21 13:28:28 +0700

    initial commit, include: Navbar, Ads)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Ads from './Ads';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Ads text="Bottom Frame" width={970} height={50} />
+    </footer>
+  );
+};
+
+export default Footer;
